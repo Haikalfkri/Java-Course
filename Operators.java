0655eac30dd6ecb8f4536285bdea10bddf918e3c
Haikal Fikri <haikaljr1245@gmail.com>
2024-05-31 06:01:24 +0700

    operators

---
+++ Operators.java
@@ -0,0 +1,32 @@
+class Operators
+{
+    public static void main(String args[]) {
+
+        int num = 7;
+        // int num2 = 5;
+        
+        // int add = num1 + num2;
+        // System.out.println(add);
+
+        // int sub = num1 - num2;
+        // System.out.println(sub);
+        
+        // int multi = num1 * num2;
+        // System.out.println(multi);
+        
+        // int division = num1 / num2;
+        // System.out.println(division);
+
+        // int remainder = num1 % num2;
+        // System.out.println(remainder);
+
+        // num = num + 2;
+        // num += 2;
+        // num *= 2;
+        // num /= 2;
+        // num++;    // increment
+        num--;    // decrement
+
+        System.out.println(num);
+    }
+}
