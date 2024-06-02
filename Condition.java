bd4cd965cad9994751ddeaf8bcfc077c777d4776
Haikal Fikri <haikaljr1245@gmail.com>
2024-06-03 05:18:01 +0700

    Condition Statements

---
+++ Condition.java
@@ -0,0 +1,61 @@
+public class Condition {
+    
+    public static void main(String args[]) {
+        
+        // int x = 10;
+
+        // if (x > 10) {
+        //     System.out.println("Hello");
+        // }
+        // else {
+        //     System.out.println("Hello");
+        // }
+
+        // int x = 25;
+
+        // if (x <= 10 && x <= 20) {
+        //     System.out.println("Hello");
+        // }
+        // else if (x <= 30) {
+        //     System.out.println("Hi");
+        // }
+        // else {
+        //     System.out.println("Bye");
+        // }
+
+        
+        // int x = 5;
+        // int y = 7;
+
+        // if (x > y) {
+        //     System.out.println(x);
+        // } else {
+        //     System.out.println(y);
+        // }
+
+        // int x = 8;
+        // int y = 7;
+        // int z = 9;
+
+        // if (x>y && x>z) {
+        //     System.out.println(x);
+        // } 
+        // else if (y>x && y>z) {
+        //     System.out.println(y);
+        // }
+        // else {
+        //     System.out.println(z);
+        // }
+
+        
+        // ternary operations ?true:false
+        int n = 5;
+        int result = 0;
+
+        result = n % 2 == 0 ? 10 : 20;
+
+        System.out.println(result);
+
+    }
+
+}
