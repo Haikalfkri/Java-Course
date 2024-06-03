241f847275c8c617f26007c94fa722c5ce02fa99
Haikal Fikri <haikaljr1245@gmail.com>
2024-06-04 05:54:38 +0700

    Loops

--- Loops.java
+++ Loops.java
@@ -1,19 +1,32 @@
 public class Loops {
     
     public static void main(String args[]) {
-
-        int i = 1;
         
-        while (i<=4) {
-            System.out.println("i: " + i);
-            int j = 1;
+        // while
+        // while (i<=4) {
+        //     System.out.println("i: " + i);
+        //     i++;
+        // }
 
-            while (j<=3) {
-                System.out.println("Hello" + j);
-                j++;
+
+        // do while
+        // do {
+        //     System.out.println("i: " + i);
+        //     i++;
+        // } while (i <= 4);
+
+        
+        // for
+        // for (int i = 4; i>=1; i--) { // decrement
+        //     System.out.println("hi " + i);
+        // }
+
+        for (int i = 1; i<=5; i++) {
+            System.out.println("Day " + i);
+
+            for (int j = 1; j <= 9; j++) {
+                System.out.println("   " + (j+8) + " - " + (j+9));
             }
-
-            i++;
         }
 
     }
