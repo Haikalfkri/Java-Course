929bfe34b0d8101299e3dcabe20fdffcdfe40c49
Haikal Fikri <haikaljr1245@gmail.com>
2024-06-05 06:53:11 +0700

    update arrays.java

--- Array.java
+++ Array.java
@@ -75,6 +75,21 @@ public class Array {
             System.out.println();
         }
         
+
+        int nums2[][] = new int[3][]; // jagged array
+
+        nums2[0] = new int[3];
+        nums2[1] = new int[4];
+        nums2[2] = new int[2];
+
+        for (int i = 0; i < nums2.length; i++) 
+        {
+            for (int j = 0; j < nums2[i].length; j++)
+            {
+                System.out.print(nums1[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
