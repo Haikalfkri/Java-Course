dee63373d3f87b1b0d8aac655a35b64d0ec32912
Haikal Fikri <haikaljr1245@gmail.com>
2024-06-20 06:11:01 +0700

    define functional interface

---
+++ functionalInterface.java
@@ -0,0 +1,23 @@
+@FunctionalInterface
+interface A
+{
+    void show();
+}
+
+
+public class functionalInterface {
+    
+    public static void main(String[] args) {
+        
+        A obj = new A() 
+        {
+            public void show()
+            {
+                System.out.println("in Show");
+            }
+        };
+        obj.show();
+
+    }
+
+}
