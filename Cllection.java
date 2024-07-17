c8908b19abc5849acf0e3c2bebb449226c8f7bd6
Haikal Fikri <haikaljr1245@gmail.com>
2024-07-18 05:24:30 +0700

    Collection

---
+++ Cllection.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Cllection
+{
+
+    public static void main(String args[])
+    {
+
+        // Collection<Integer> nums = new ArrayList<Integer>();
+        Collection nums = new ArrayList();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+
+        // for ( int n : nums ) {
+        //     System.out.println(n);
+        // }
+
+    }
+
+}
--- Threads.java
+++ Threads.java
@@ -1,48 +1,69 @@
-class A implements Runnable // extends Thread
-{
-    public void run()
-    {
-        for (int i = 1; i<=10; i++)
-        {
-            System.out.println("hi");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
+// class A implements Runnable // extends Thread
+// {
+//     public void run()
+//     {
+        
+//     }
+// }
 
-class B implements Runnable  // extends Thread
-{
-    public void run()
-    {
-        for (int i = 1; i<=10; i++)
-        {
-            System.out.println("hello");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
+// class B implements Runnable  // extends Thread
+// {
+//     public void run()
+//     {
+        
+//     }
+// }
 
+class Counter 
+{
+    int count;
+    public void increment() {
+        count++;
+    }
+
+}
 public class Threads
 {
 
     public static void main(String[] args) {
 
-        Runnable obj1 = new A();
-        Runnable obj2 = new B();
+        Counter c = new Counter();
+
+        Runnable obj1 = () -> {
+            for (int i = 1; i<=100; i++)
+            {
+                // System.out.println("hi");
+                // try {
+                //     Thread.sleep(10);
+                // } catch (InterruptedException e) {
+                //     e.printStackTrace();
+                // }
+                c.increment();
+            }
+        };
+        Runnable obj2 = () -> {
+            for (int i = 1; i<=100; i++)
+            {
+                // System.out.println("hello");
+                // try {
+                //     Thread.sleep(10);
+                // } catch (InterruptedException e) {
+                //     e.printStackTrace();
+                // }
+                c.increment();
+            }
+        };
 
         // obj1.start();
         // obj2.start();
         
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+
+        System.out.println(c.count);
 
     }
 
