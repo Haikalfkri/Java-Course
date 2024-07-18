134aae6673f5e177946ff794af00a20c79c8cc93
Haikal Fikri <haikaljr1245@gmail.com>
2024-07-19 05:22:11 +0700

    LVTI ( local variable type inference )

---
+++ LVTI.java
@@ -0,0 +1,16 @@
+class LVTI
+{
+    public static void main(String args[])
+    {
+        int a = 9;
+        var b = 8;
+        String var = "Test";
+
+        int c;
+        var d = 10;
+
+        //int nums[] = new int[10];
+
+        var nums= new int[10];
+    }
+}
